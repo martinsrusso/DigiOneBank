ce39f691126c7b727b62688c4142ab3072a3c511
Leonardo Nascimento <leonardo.nascimento@outlook.com>
2021-04-14 15:58:00 -0300

    Criação da classe pessoa

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -0,0 +1,12 @@
+package one.digitalinnovation.digionebank
+
+class Pessoa {
+    var nome: String = "Leo"
+    var cpf: String = "111.111.111-11"
+}
+
+fun main() {
+    val _pessoa = Pessoa()
+    println(_pessoa.nome)
+    println(_pessoa.cpf)
+}
