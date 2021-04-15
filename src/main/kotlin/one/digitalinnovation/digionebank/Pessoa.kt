7af6c52ac2e83e2d4a2d085923f5e7da6a3c4615
Leonardo Nascimento <leonardo.nascimento@outlook.com>
2021-04-15 15:41:43 -0300

    updated to make person and employee abstract class

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,18 +1,6 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome: String = "Leo"
-    var cpf: String = "111.111.111-11"
-    private set
-
-    constructor()
-
-    fun pessoaInfo() = "Nome:$nome - CPF:$cpf"
-}
-
-fun main() {
-    val _pessoa = Pessoa()
-
-    println(_pessoa)
-    println(_pessoa.pessoaInfo())
-}
+abstract class Pessoa (
+    val nome: String,
+    val cpf: String
+)
