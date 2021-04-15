8af08b25e21b82c897126c3fb1e13618711bbebe
Leonardo Nascimento <leonardo.nascimento@outlook.com>
2021-04-15 10:58:46 -0300

    Exemplo de inner class

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,10 +3,16 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Leo"
     var cpf: String = "111.111.111-11"
+
+    inner class Endereco
+    {
+        var rua: String = "Rua 1"
+    }
 }
 
 fun main() {
     val _pessoa = Pessoa()
     println(_pessoa.nome)
     println(_pessoa.cpf)
+    println(_pessoa.Endereco().rua)
 }
